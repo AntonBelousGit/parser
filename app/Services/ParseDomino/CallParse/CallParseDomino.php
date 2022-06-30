4f434184008ef9494bcb813a769a3aa75c74313c
AntonBelousGit <antonbelouswork@gmail.com>
2022-06-30 17:33:07 +0300

    DominoParse work

--- app/Services/ParseDomino/CallParse/CallParseDomino.php
+++ app/Services/ParseDomino/CallParse/CallParseDomino.php
@@ -3,33 +3,21 @@
 
 namespace App\Services\ParseDomino\CallParse;
 
-use App\Services\BaseServices\FlavorService\Contracts\FlavorServiceContract;
 use App\Services\BaseServices\ParserProductData;
-use App\Services\BaseServices\SizeService\Contracts\SizeServiceContract;
-use App\Services\BaseServices\ToppingService\Contracts\ToppingServiceContract;
 use App\Services\ParseDomino\ParserService\Contracts\DominoParseServiceAttributeContract;
 use App\Services\ParseDomino\ParserService\Contracts\DominoParseServiceContract;
-use App\Services\ParseDomino\ProductService\Contracts\ProductServiceContract;
 use Throwable;
 
 class CallParseDomino
 {
     /**
      * CallParseDomino constructor.
-     * @param SizeServiceContract $sizeServiceContract
-     * @param FlavorServiceContract $flavorServiceContract
-     * @param ToppingServiceContract $toppingServiceContract
      * @param DominoParseServiceContract $contract
      * @param DominoParseServiceAttributeContract $attributeContract
-     * @param ProductServiceContract $productServiceContract
      */
     public function __construct(
-        public SizeServiceContract $sizeServiceContract,
-        public FlavorServiceContract $flavorServiceContract,
-        public ToppingServiceContract $toppingServiceContract,
         public DominoParseServiceContract $contract,
         public DominoParseServiceAttributeContract $attributeContract,
-        public ProductServiceContract $productServiceContract
     ) {
     }
 
