8d547a1c183024f747abb93f20de64cfbd22973e
AntonBelousGit <antonbelouswork@gmail.com>
2022-07-11 19:00:41 +0300

    Update parse

--- config/parsers.php
+++ config/parsers.php
@@ -2,26 +2,25 @@
 declare(strict_types=1);
 
 use App\Services\ParserManager\Drivers\DominoParseDriver;
+use App\Services\ParserManager\Drivers\OrigamiPizzaParseDriver;
 use App\Services\ParserManager\Drivers\VdhPizzaParseDriver;
 use App\Services\ParserManager\Drivers\ZharPizzaParseDriver;
 
 return [
-    'dominoParse' => [
+    [
         'parser' => DominoParseDriver::class,
-        'config' => [
-            'address' => 'https://dominos.ua/uk/chornomorsk/',
-        ],
+        'url' => 'https://dominos.ua/uk/chornomorsk/',
     ],
-    'zharPizza' => [
+    [
         'parser' => ZharPizzaParseDriver::class,
-        'config' => [
-            'address' => 'https://store.tildacdn.com/api/getproductslist/?storepartuid=261323000731&recid=264435121&c=1656336574041&getparts=true&getoptions=true&slice=1&&size=36',
-        ],
+        'url' => 'https://store.tildacdn.com/api/getproductslist/?storepartuid=261323000731&recid=264435121&c=1656336574041&getparts=true&getoptions=true&slice=1&&size=36',
     ],
-    'vdhBar' => [
+    [
         'parser' => VdhPizzaParseDriver::class,
-        'config' => [
-            'address' => 'https://store.tildacdn.com/api/getproductslist/?storepartuid=261323000731&recid=264435121&c=1655380264126&getparts=true&getoptions=true',
-        ],
+        'url' => 'https://store.tildacdn.com/api/getproductslist/?storepartuid=261323000731&recid=264435121&c=1655380264126&getparts=true&getoptions=true',
     ],
+//    [
+//        'parser' => OrigamiPizzaParseDriver::class,
+//        'url' => 'https://origami.od.ua/index.php?route=product/category&path=68',
+//    ],
 ];
