3ef8f4dfcd3d790b5095bd717e011b6e07b4339e
AntonBelousGit <antonbelouswork@gmail.com>
2022-07-21 11:48:08 +0300

    Update TestDataService

--- app/Services/TestDataService/TestDataService.php
+++ app/Services/TestDataService/TestDataService.php
@@ -5,24 +5,14 @@ declare(strict_types=1);
 namespace App\Services\TestDataService;
 
 use App\Models\ParseConfig;
-use App\Services\ConnectToParseService\Contracts\ConnectToParseServiceContract;
 use App\Services\ParserManager\ParseManager;
 use App\Services\TestDataService\Contracts\TestDataServiceContract;
-use File;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Log;
 use Throwable;
 
 class TestDataService implements TestDataServiceContract
 {
-    /**
-     * TestDataService constructor.
-     * @param ConnectToParseServiceContract $parseServiceContract
-     */
-    public function __construct(private ConnectToParseServiceContract $parseServiceContract)
-    {
-    }
-
     /**
      * Test valid config and parsed data
      */
@@ -32,12 +22,6 @@ class TestDataService implements TestDataServiceContract
             $configOld = ParseConfig::where('enable', 1)->get();
             $parsingManager = app(ParseManager::class);
             $parsingManager->callParse(Collection::make($configOld));
-
-            $configNew = ParseConfig::where('enable', 1)->get();
-            foreach ($configNew as $parser) {
-                $html = $this->parseServiceContract->connect($parser['connection'], $parser['url']);
-                File::put(storage_path("/app/public/file/{$parser['name']}.xml"), $html);
-            }
         } catch (Throwable $exception) {
             Log::info('parseToFile - problem ' . $exception);
         }
