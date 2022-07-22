da57d8f87ead80d2b1c5edcce03c20de8fa8249c
AntonBelousGit <antonbelouswork@gmail.com>
2022-07-22 17:29:54 +0300

    Update phpDoc

--- tests/Unit/ParserManagerTests/ParseManagerTest.php
+++ tests/Unit/ParserManagerTests/ParseManagerTest.php
@@ -8,6 +8,7 @@ use App\Services\ConnectToParseService\ConnectToParseService;
 use App\Services\ParserManager\DTOs\ParserProductDataDTO;
 use App\Services\ParserManager\ParseManager;
 use DiDom\Document;
+use File;
 use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Mockery;
@@ -20,37 +21,71 @@ class ParseManagerTest extends TestCase
     /**
      * @throws BindingResolutionException
      */
-    public function testGetProductDataFromParsedPage()
+    public function testParserDominoParse()
     {
-        $response = $this->parse();
+        $response = $this->parse('dominoParse', 'DiDom');
         $this->assertNotEmpty($response);
         $this->assertNotEmpty($response->products[0]);
-    }
-
-    /**
-     * @throws BindingResolutionException
-     */
-    public function testGetAttributeDataFromParsedPage()
-    {
-        $response = $this->parse();
         $this->assertNotNull($response->attributes->size[0]);
         $this->assertNotNull($response->attributes->topping[0]);
         $this->assertNotNull($response->attributes->flavor[0]);
     }
 
     /**
+     * @throws BindingResolutionException
+     */
+    public function testZharPizzaParse()
+    {
+        $response = $this->parse('zharPizza');
+        $this->assertNotEmpty($response);
+        $this->assertNotEmpty($response->products[0]);
+        $this->assertNotNull($response->attributes->size[0]);
+        $this->assertNotNull($response->attributes->topping[0]);
+    }
+
+    /**
+     * @throws BindingResolutionException
+     */
+    public function testVdhBarParse()
+    {
+        $response = $this->parse('vdhBar');
+        $this->assertNotEmpty($response);
+        $this->assertNotEmpty($response->products[0]);
+        $this->assertNotNull($response->attributes->size[0]);
+        $this->assertNotNull($response->attributes->topping[0]);
+    }
+
+    /**
+     * @throws BindingResolutionException
+     */
+    public function testOrigamiParse()
+    {
+        $response = $this->parse('origami', 'DiDom');
+        $this->assertNotEmpty($response);
+        $this->assertNotEmpty($response->products[0]);
+        $this->assertNotNull($response->attributes->size[0]);
+        $this->assertNotNull($response->attributes->topping[0]);
+    }
+
+    /**
+     * @param string $parseName
+     * @param string $type
      * @return ParserProductDataDTO
      * @throws BindingResolutionException
      */
-    protected function parse(): ParserProductDataDTO
+    protected function parse(string $parseName, string $type = ''): ParserProductDataDTO
     {
-        $config = config('parsers.dominoParse');
-        $document = new Document(storage_path('app/public/file/dominoParse.xml'), true);
+        $config = config("parsers.{$parseName}");
+        if ($type === 'DiDom') {
+            $document = new Document(storage_path("app/public/file/{$parseName}.xml"), true);
+        } else {
+            $document = File::get(storage_path("app/public/file/{$parseName}.xml"));
+        }
         $mock = Mockery::mock(ConnectToParseService::class)->makePartial();
         $mock->shouldReceive('connect')->andReturns($document);
         app()->instance(ConnectToParseService::class, $mock);
 
         $parsingManager = app(ParseManager::class);
-        return $parsingManager->callParse($config);
+        return $parsingManager->callParse($config['parser'], $config['url']);
     }
 }
