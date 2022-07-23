9a23995e1bafb304723854c27234de8d5e00f743
AntonBelousGit <antonbelouswork@gmail.com>
2022-07-23 16:39:16 +0300

    Rename command file

--- app/Console/Commands/ProductParseAndStoreCommand.php
+++ app/Console/Commands/ProductParseAndStoreCommand.php
@@ -4,35 +4,35 @@ declare(strict_types=1);
 
 namespace App\Console\Commands;
 
-use App\Jobs\ParseStoreInFileJob;
+use App\Jobs\ParseAndStoreProductJob;
 use Illuminate\Console\Command;
 
-class TestDataParse extends Command
+class ProductParseAndStoreCommand extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'parse-test';
+    protected $signature = 'parse';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command to start testing parsing data';
+    protected $description = 'Command to start parsing product';
 
     /**
      * Execute the console command.
      *
      * @return void
      */
-    public function handle():void
+    public function handle()
     {
         $configs = config('parsers');
-        foreach ($configs as $key => $config) {
-            dispatch(new ParseStoreInFileJob($key, $config));
+        foreach ($configs as $config) {
+            dispatch(new ParseAndStoreProductJob($config));
         }
     }
 }
--- app/Console/Commands/StoreParsedDataToFileCommand.php
+++ app/Console/Commands/StoreParsedDataToFileCommand.php
@@ -4,35 +4,35 @@ declare(strict_types=1);
 
 namespace App\Console\Commands;
 
-use App\Jobs\ParseAndStoreProductJob;
+use App\Jobs\ParseStoreInFileJob;
 use Illuminate\Console\Command;
 
-class ProductParse extends Command
+class StoreParsedDataToFileCommand extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'parse';
+    protected $signature = 'parse-test';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command to start parsing product';
+    protected $description = 'Command to start testing parsing data';
 
     /**
      * Execute the console command.
      *
      * @return void
      */
-    public function handle()
+    public function handle():void
     {
         $configs = config('parsers');
-        foreach ($configs as $config) {
-            dispatch(new ParseAndStoreProductJob($config));
+        foreach ($configs as $key => $config) {
+            dispatch(new ParseStoreInFileJob($key, $config));
         }
     }
 }
